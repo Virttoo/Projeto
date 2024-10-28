b044ff143f72f29eea34bbdbf0e702174fde263e
victorsr675 <victor_s.rodrigues@hotmail.com>
2024-10-28 12:18:08 -0300

    image correction

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets.avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
     img.setAttribute(
       "alt",
       "Foto de Mayk Brito sorrindo, usando òculos e camisa preta, sem barba e fundo azul."
     )
   } else {
     //se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets.avatar.png")
+    img.setAttribute("src", "./assets/avatar.png")
     img.setAttribute(
       "alt",
       "Foto de Mayk Brito sorrindo, usando òculos e camisa preta, barba e fundo amarelo."
